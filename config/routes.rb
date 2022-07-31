0bdb17b0b953cf1b317c70514ae30405cf87dff8
Oswaldo-Hqz <oehero@hotmail.com>
2022-07-31 14:36:38 -0600

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      resources :books
+      resources :borrowings
+      resources :genres
+      resources :roles
+      resources :users, only: [:index, :show, :create]
+      post "/login", to: "auth#login"
+      get "/auto_login", to: "auth#auto_login"
+      get "/user_is_authed", to: "auth#user_is_authed"
+    end
+  end
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "root#index"
 end
