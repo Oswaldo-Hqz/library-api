64639cb972b47e03f783cbc742a48a261ddf55ef
Oswaldo-Hqz <oehero@hotmail.com>
2022-07-31 22:17:04 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+
       resources :books
-      resources :borrowings
       resources :genres
       resources :roles
-      resources :users, only: [:index, :show, :create]
+
+      resources :borrowings, only: [:index, :show, :create, :update, :destroy]
+      post "/return_book", to: "borrowings#return_book"
+
+      resources :users, only: [:index, :create]
+      get "/profile/:id", to: "users#profile"
+      get "/user_by_Email", to: "users#user_by_Email"
       post "/login", to: "auth#login"
       get "/auto_login", to: "auth#auto_login"
       get "/user_is_authed", to: "auth#user_is_authed"
